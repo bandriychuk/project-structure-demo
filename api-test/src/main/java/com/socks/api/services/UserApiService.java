86dfb91663ffd73efcedd19ff95130b6434e23f5
Andriychuk Borys <andriychuk23@gmail.com>
2021-04-01 21:24:24 +0300

    Added UserApiService

--- api-test/src/main/java/com/socks/api/services/UserApiService.java
+++ api-test/src/main/java/com/socks/api/services/UserApiService.java
@@ -10,7 +10,7 @@ public class UserApiService extends ApiService {
 	public AssertableResponse registerUser(UserPayload user) {
 		return new AssertableResponse(setUp()
 				.body(user)
-				.when().post("register")
+				.when().post("/register")
 				.then().extract().response());
 	}
 }
