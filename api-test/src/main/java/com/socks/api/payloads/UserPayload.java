4a275cca5371a15fe6f836d068c7bd0577fa74e5
Andriychuk Borys <andriychuk23@gmail.com>
2021-04-01 19:52:17 +0300

    Created UserPayload

---
+++ api-test/src/main/java/com/socks/api/payloads/UserPayload.java
@@ -0,0 +1,40 @@
+package com.socks.api.payloads;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+import lombok.experimental.Accessors;
+
+
+@Getter
+@Setter
+@ToString
+@Accessors(fluent = true)
+public class UserPayload{
+
+	@JsonProperty("firstName")
+	private String firstName;
+
+	@JsonProperty("lastName")
+	private String lastName;
+
+	@JsonProperty("password")
+	private String password;
+
+	@JsonProperty("userStatus")
+	private int userStatus;
+
+	@JsonProperty("phone")
+	private String phone;
+
+	@JsonProperty("id")
+	private String id;
+
+	@JsonProperty("email")
+	private String email;
+
+	@JsonProperty("username")
+	private String username;
+
+}
