68fa2bc4ab22b520b936b4ac2719bd573b26b45c
Andriychuk Borys <andriychuk23@gmail.com>
2021-04-02 11:19:56 +0300

    Update CartApiService

--- api-test/src/main/java/com/socks/api/services/CartApiService.java
+++ api-test/src/main/java/com/socks/api/services/CartApiService.java
@@ -1,15 +1,35 @@
 package com.socks.api.services;
 
+
+import com.socks.api.assertions.AssertableResponse;
+
 import java.util.HashMap;
 import java.util.Map;
 
-public class CartApiService {
+public class CartApiService extends ApiService{
 
 	private Map<String, String> getCookie(String sid) {
 		Map<String, String> cookie = new HashMap<>();
-		cookie.put("mk.dir", sid);
+		cookie.put("", sid);
 		return cookie;
 	}
 
+	public AssertableResponse addItemToCart(String id, String sid) {
+		return new AssertableResponse(
+				setUp()
+				.cookies(getCookie(sid))
+				.when()
+				.body("{\"id\":\"" + id + "\"}")
+				.post("/cart")
+		);
+	}
+
+	public AssertableResponse getCartItems(String sid) {
+		return new AssertableResponse(
+				setUp()
+						.cookies(getCookie(sid))
+						.when()
+						.get("/cart"));
+	}
 
 }
