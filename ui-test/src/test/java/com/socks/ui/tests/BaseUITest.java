44df58d994d03e90553a5516e272f5954621fab8
Andriychuk Borys <andriychuk23@gmail.com>
2021-04-02 11:20:08 +0300

    Update BaseUITest

--- ui-test/src/test/java/com/socks/ui/tests/BaseUITest.java
+++ ui-test/src/test/java/com/socks/ui/tests/BaseUITest.java
@@ -5,18 +5,18 @@ import com.codeborne.selenide.Selenide;
 import com.socks.api.ProjectConfig;
 import io.restassured.RestAssured;
 import org.aeonbits.owner.ConfigFactory;
-import org.testng.annotations.BeforeSuite;
+import org.testng.annotations.Test;
 
 public class BaseUITest {
 
-	@BeforeSuite
-	public void setUp() {
+	@Test
+	public void setUp(){
 		ProjectConfig config = ConfigFactory.create(ProjectConfig.class);
 		RestAssured.baseURI = config.baseUrl();
-		Configuration.baseUrl = config.baseUrl();
+		Configuration.baseUrl  = "chrome";
 	}
 
-	protected <T> T at(Class<T> pageClass) {
+	protected <T> T at(Class <T> pageClass) {
 		return Selenide.page(pageClass);
 	}
 }
