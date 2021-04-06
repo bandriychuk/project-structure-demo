018e0a5155f43d345b7433c18164c284e04233e3
Andriychuk Borys <andriychuk23@gmail.com>
2021-04-06 12:17:00 +0300

    Added SelenoidDriverProvider

--- ui-test/src/test/java/com/socks/ui/tests/BaseUITest.java
+++ ui-test/src/test/java/com/socks/ui/tests/BaseUITest.java
@@ -5,15 +5,15 @@ import com.codeborne.selenide.Selenide;
 import com.socks.api.ProjectConfig;
 import io.restassured.RestAssured;
 import org.aeonbits.owner.ConfigFactory;
-import org.testng.annotations.Test;
+import org.testng.annotations.BeforeClass;
 
 public class BaseUITest {
 
-	@Test
+	@BeforeClass
 	public void setUp(){
 		ProjectConfig config = ConfigFactory.create(ProjectConfig.class);
 		RestAssured.baseURI = config.baseUrl();
-		Configuration.baseUrl  = "chrome";
+		Configuration.browser  = "com.socks.ui.SelenoidDriverProvider";
 	}
 
 	protected <T> T at(Class <T> pageClass) {
